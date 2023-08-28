c7e67267437ae7c9b739f7801de8d781232b2a74
RaiTGames <117438915+RaiTGames@users.noreply.github.com>
2023-08-28 23:45:28 +0900

    Add files via upload

---
+++ TETR!S.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Tetris Game</title>
+<style>
+  /* CSS styles for the game elements */
+</style>
+</head>
+<body>
+<div id="game-board"></div>
+<script>
+  // JavaScript code for the Tetris game
+</script>
+</body>
+</html>
